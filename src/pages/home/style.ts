5879dae64cda58a05cb2acd189160205506ed4e2
jequelia <jequeliasantana@gmail.com>
2021-05-29 20:41:51 -0300

    delete repo

--- src/pages/home/style.ts
+++ src/pages/home/style.ts
@@ -1,111 +1,123 @@
-import styled, {css} from 'styled-components';
-import {shade} from 'polished';
+import styled, { css } from "styled-components";
+import { shade } from "polished";
 
-interface FormProps{
-    hasError: boolean;
+interface FormProps {
+  hasError: boolean;
 }
 
-export const Title = styled.h1 `
-    font-size: 48px;
-    color: #3a3a3a;
-    max-width: 650px;
-    line-height: 56px;
-    margin-top: 80px;
-`
+export const Title = styled.h1`
+  font-size: 48px;
+  color: #3a3a3a;
+  max-width: 650px;
+  line-height: 56px;
+  margin-top: 80px;
+`;
 
 export const Form = styled.form<FormProps>`
+  margin-top: 40px;
+  max-width: 700px;
+  display: flex;
 
-    margin-top: 40px;
-    max-width: 700px;
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+
+    ${(props) =>
+      props.hasError &&
+      css`
+        border: 1px red solid;
+      `}
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    width: 200px;
+    height: 70px;
+    background-color: #00d25a;
+    border-radius: 0 5px 5px 0;
+    border: 0;
+    color: #fff;
+    font-weight: bold;
+    transition: 0.5s;
+
+    &:hover {
+      transition: 0.5s;
+      background: ${shade(0.2, "#00D25A")};
+    }
+  }
+`;
+
+export const Container = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  margin-top: 30px;
+  max-width: 700px;
+`;
+
+export const Repository = styled.form`
+  #delete {
+    margin-left: 3%;
+    color: #ff9800;
+    &:hover {
+      transition: 0.5s;
+      color: ${shade(0.2, "#ff5722")};
+      cursor: pointer;
+    }
+  }
+
+  a {
+    background-color: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
     display: flex;
+    text-decoration: none;
+    align-items: center;
+    transition: 0.5s;
 
-    input {
-        flex: 1;
-        height: 70px;
-        padding: 0 24px;
-        border: 0;
-        border-radius: 5px 0 0 5px;
-
-        ${(props) => props.hasError && css `
-            border: 1px red solid ;
-        
-        `}
-
-        &::placeholder{
-            color: #a8a8b3;
-        }
+    &:hover {
+      transform: translateX(10px);
     }
 
-    button{
-        width: 200px;
-        height: 70px;
-        background-color: #00D25A;
-        border-radius: 0 5px 5px 0;
-        border: 0;
-        color: #FFF;
-        font-weight: bold;
-        transition: 0.5s;
-
-        &:hover{
-            transition: 0.5s;
-            background: ${shade(0.2, '#00D25A' )}
-        }
+    & + a {
+      margin-top: 18px;
     }
 
-`
-
-export const Repository = styled.form `
-    margin-top: 80px;
-    max-width: 700px;
-
-    a{
-        background-color: #FFF;
-        border-radius: 5px;
-        width: 100%;
-        padding: 24px;
-        display: flex;
-        text-decoration: none;
-        align-items: center;
-        transition: 0.5s;
-
-        &:hover{
-            transform: translateX(10px);
-        }
-
-        & + a {
-            margin-top: 18px;
-        }
-
-        img{
-            width: 65px;
-            height: 65px;
-            border-radius: 50%50%;
-        }
-
-        div{
-            margin-left: 16px;
-
-            strong {
-                font-size: 20px;
-                color: #3D3D4D;
-            }
-            p{
-                font-size: 18px;
-                color: #3D3D4D;
-            }
-        }
-
-        svg{
-            margin-left: auto;
-            color: #cbcbd6;
-        }
-
+    img {
+      width: 65px;
+      height: 65px;
+      border-radius: 50%50%;
     }
 
-`
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+      p {
+        font-size: 18px;
+        color: #3d3d4d;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
+`;
 export const Error = styled.span`
-    display: block;
-    color: red;
-    margin-top: 10px;
-    
-`
+  display: block;
+  color: red;
+  margin-top: 10px;
+`;
--- src/pages/repository/index.tsx
+++ src/pages/repository/index.tsx
@@ -9,120 +9,6 @@ interface RepositoryParam {
   repository: string;
 }
 
-<<<<<<< HEAD
-interface RepositoryInterface{
-    repository: string;
-    full_name: string;
-    description: string;
-    forks_count: number;
-    stargazers_count: number;
-    open_issues_count: number;
-
-    owner:{
-        login: string;
-        avatar_url: string;
-    }
-}
-
-interface  Issue{
-    id: number,
-    title: string,
-    html_url: string,
-    user:{
-        login: string,
-    }
-
-}
-
-const Repository: React.FunctionComponent = () => {
-
-    const [ repository, setRepository] = useState<RepositoryInterface | null>(null);
-    const [ issues, setIssues] = useState<Issue[]>([]);
-    const {params} = useRouteMatch<RepositoryParam>();
-
-    useEffect(()=>{
-
-        api.get(`repos/${params.repository}`).then(
-            response => {
-                console.log(response.data);
-                setRepository(response.data);
-            }
-        )
-
-        api.get(`repos/${params.repository}/issues`).then(
-            response => {
-                console.log(response.data);
-                setIssues(response.data)
-            }
-        )
-
-        // async function loadData(): Promise<void> {
-        //     const [ repository, issues] = await Promise.all([
-        //         api.get(`repos/${params.repository}`),
-        //         api.get(`repos/${params.repository}/issues`)
-        //     ])
-
-        //     console.log('repository', repository);
-        //     console.log('issues', issues.data);
-            
-        // }
-
-        // loadData()
-
-    }, [params.repository])
-
-    return (
-        <>
-            <Header>
-                <img src={Logo} alt="Logo App"/>
-                <Link to="/"> <FiChevronLeft size={30}/>Voltar</Link>
-            </Header>
-
-            { (
-                 <InfoRepo>
-                 <header>
-                     <img 
-                         src={repository?.owner.avatar_url}
-                         alt={repository?.full_name}
-                     />
-                     <div>
-                         <strong>{repository?.full_name}</strong>
-                         <p>{repository?.description}</p>
-                     </div>
-                 </header>
-               
-                 <ul>
-                     <li>
-                         <strong>{repository?.stargazers_count}</strong>
-                         <span>Start</span>
-                     </li>
-                     <li>
-                         <strong>{repository?.forks_count}</strong>
-                         <span>Forks</span>
-                     </li>
-                     <li>
-                         <strong>{repository?.open_issues_count}</strong>
-                         <span>Issues abertas</span>
-                     </li>
-                 </ul>
-             </InfoRepo>
-            )}
-           
-            <Issues>
-                {issues.map( issue =>(
-                     <a key={issue.id} href={issue.html_url}>
-                     <div>
-                         <strong>{issue.title}</strong>
-                         <p>{issue.user.login}</p>
-                     </div>
-                     <FiChevronRight size={40}></FiChevronRight>
-                     </a>
-                ))}
-            </Issues>
-            
-        </>
-    );
-=======
 interface Repository {
   repository: string;
   full_name: string;
@@ -144,7 +30,6 @@ interface Issue {
   user: {
     login: string;
   };
->>>>>>> a76fd94a2518c8b430bbab56d2370d1187294b32
 }
 
 const Repository: React.FunctionComponent = () => {

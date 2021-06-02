084efbca305ad60154a7146fe8fab75754bd6d41
James <jamesmarcos2123@hotmail.com>
2021-06-02 19:31:51 -0300

    added test in repository

---
+++ test/test.js
@@ -0,0 +1,36 @@
+import { Selector } from 'testcafe';
+
+fixture`Getting Started`
+    .page`https://jamesmarcos2123.github.io/`;
+
+test('usuario/repository existente', async t => {
+    await t
+        .typeText('#input-search', 'DevExpress/testcafe')
+        .click('#button-submit')
+        .expect(Selector('.Toastify').innerText).eql('adicionado com sucesso');;
+
+
+});
+
+
+
+test('usuario/repository não existente', async t => {
+    await t
+        .typeText('#input-search', 'DevExpress/selenium')
+        .click('#button-submit')
+        .expect(Selector('.Toastify').innerText).eql('deu erro');;
+
+
+});
+
+test('usuario/repository duplicado', async t => {
+    await t
+        .typeText('#input-search', 'DevExpress/selenium')
+        .click('#button-submit')
+        .typeText('#input-search', 'DevExpress/selenium')
+        .click('#button-submit').expect(Selector('.Toastify').innerText).eql('');
+
+
+});
+
+
